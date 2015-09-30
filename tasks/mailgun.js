203403db4c27529a5a32bfba8bbd199c9aa648cc
markhuge <mark@segfawlt.net>
2015-09-30 06:16:02 -0700

    refactor 2 year old code

--- tasks/mailgun.js
+++ tasks/mailgun.js
@@ -9,15 +9,14 @@
 /*jslint node: true */
 'use strict';
 
-var mailer = require('mailgun-send'),
-    clone  = require('lodash.clone'),
-    pick   = require('lodash.pick'),
-    crypto = require('crypto');
+var mail  = require('../lib/mail.js'),
+    clone = require('lodash.clone');
 
 var task = {
   name: 'mailgun',
   description: 'Send emails through Mailgun as part of your build'
 };
+
 
 module.exports = function (grunt) {
 
@@ -25,54 +24,26 @@ module.exports = function (grunt) {
     
     var done   = this.async(),
         opts   = this.data.options,
-        params = buildParams(opts),
         count  = this.filesSrc.length;
-
-
-    // Register our mailer instance with out API key
-    mailer.config({ key: opts.key });
     
-    // If there's no files, just send params
-    if (count < 1) { return send(params, function () { done(); }); }
+    opts.log = grunt.log.writeln;
+
+    if (count < 1) { return mail(opts, function () { 
+      done(); });
+    }
     
     // Otherwise iterate over files
     this.filesSrc.forEach(function (filepath) {
-      var options = clone(params);
+      var options = clone(opts);
      
-      // Attempt to prevent email client conversation threading
-      if (opts.preventThreading) { options = preventThreading(options); }
       options.file = filepath;
       options.body = grunt.file.read(filepath);
-      send(options, function () {
+      mail(options, function () {
         count--;
         if (count < 1)  { return done(); }
       });
     });
       
   });
-
-  function send (opts, cb) {
-    mailer.send(opts, function (err) {
-      var rcp = '';
-      if (err) { return grunt.log.error(err); }
-      var msg = opts.file || 'mailgun msg';
-      if (!opts.hideRecipient) { rcp = ' to ' + opts.recipient; }
-      grunt.log.writeln('Sent ' + msg + rcp);
-      if (cb) { cb(); }
-    });
-  }
-
-  function buildParams (obj) {
-    obj.subject = obj.subject || 'grunt-mailgun';
-    obj.body    = obj.body   || 'grunt-mailgun';
-    return pick(obj, [ 'sender', 'recipient', 'subject', 'body' ]);
-  }
-
-  function preventThreading (obj) {
-    var pad = crypto.randomBytes(10).toString('hex');
-    obj.subject += ' - ' + pad;
-    obj.headers = "\nIn-Reply-To: <" + pad + ">";
-    return obj;
-  }
 };
 
